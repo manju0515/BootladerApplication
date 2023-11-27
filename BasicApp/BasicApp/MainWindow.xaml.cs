e0baafc1e2aa41d6730ba46335d9d0968eb98984
manju0515 <101547284+manju0515@users.noreply.github.com>
2023-11-27 09:47:29 +0530

    added wpf application project

---
+++ BasicApp/BasicApp/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Peak.Can.Basic;
+namespace WpfTransmissionApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSendButtonClick(object sender, RoutedEventArgs e)
+        {
+            // Get the selected CAN channel from the ComboBox
+            string selectedCanChannel = (canChannelsComboBox.SelectedItem as ComboBoxItem)?.Content.ToString();
+
+            // Get the message from the TextBox
+            string message = messageTextBox.Text;
+
+           
+            // Display the transmitted message and selected CAN channel in the Label.
+            transmittedMessageLabel.Content = $"Message Transmitted on {selectedCanChannel}: {message}";
+        }
+
+        private void OnTextBoxMouseDoubleClick(object sender, RoutedEventArgs e)
+        {
+            // Clear the text when the TextBox is double-clicked
+            if (sender is TextBox textBox)
+            {
+                textBox.Text = string.Empty;
+            }
+        }
+    }
+}
